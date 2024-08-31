8e70efbaa136d138a66aa5a429063ab8613b7f45
iltherion <iltherion@gmx.fr>
2024-08-31 23:20:05 +0200

    Gestion du 0 ou de lettres en argument

--- main.c
+++ main.c
@@ -6,12 +6,18 @@
 int main(int argc , char *argv[]  )
 {
 	
+		
 	erreur_argc(verif_argc(argc,3));	
-	
+		
+	if (atoi(argv[1])== 0 || atoi(argv[2])==0)
+		goto fin ;
 	printf ("Le nombre d'argument : %d \n",atoi(argv[1]));
 	printf("Le pgcd de %d et %d est: %d\n",atoi(argv[1]),atoi(argv[2]),pgcd(atoi(argv[1]),atoi(argv[2])));	
 
 
-
+	fin :
+		fprintf( stderr, "Vous avez mis un 0 ou un mot en argument\n");
+		return 3;
+	
 	return 0;
 }
