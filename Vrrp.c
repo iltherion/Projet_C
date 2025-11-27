e585d75177e1e126af91adbfe32bc5c14e598f5c
iltherion <68922565+iltherion@users.noreply.github.com>
2025-11-27 23:18:00 +0100

    Ajout des options

--- Vrrp.c
+++ Vrrp.c
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdint.h>  //uint
 #include <stdlib.h>
@@ -39,21 +40,90 @@ typedef struct _vrrphdr {			/* rfc2338.5.1 */
 	uint8_t			auth_type;	/* authentification type */
 	uint8_t 		adver_int;	/* advertisement interval (in sec) */
 	uint16_t		chksum;		/* checksum (ip-like one) */
-	/* here <naddr> ip addresses */
-	/* here authentification infos */
+	double			bourrage; 	/* Bit de bourrage */
 } vrrphdr_t;
 
+void help(char *nom_programme){
+	printf("Utilisation: \n\
+	%s --ip <ip_dst>\n\
+\n\
+\n\
+---------------------------------------------------------\n\
+Option : \n\
+	--ip  ou -i  			==> ip destination du paquet\n\
+	--vrid	 	defaut 100	==> groupe vrrp entre 0 et 255\n\
+	--prio 		defaut 50	==> Priorité vrrp entre 1 et 255\n\
+	--count	 	defaut 1	==> Nombre de paquet envoyé\n\
+	--time    	defaut 1	==> Temps entre chaque envoye de paquet\n\
+	--verbose ou -v			==> Affiche des infos \n\
+	--help ou -h			==> L'aide de la commande\n\
+\n\
+---------------------------------------------------------\n"
+,nom_programme);
+}
 
+void option ( int argc , char * argv[] , char ip[] , char * vrid , char * priority , int * nb_p , int * time , int * verbose) {
+	
+	int opt	;
+	int option_index = 0 ;
+	static struct option long_options[] = {
+		{"ip", required_argument, 0 , 'i'},
+		{"vrid" , required_argument, 0 , 'r'},
+		{"prio" , required_argument, 0 , 'p'},
+		{"count" , required_argument, 0 , 'c'},
+		{"time" , required_argument, 0 , 't'},
+		{"verbose" , no_argument, 0 , 'v'},
+		{"help" , no_argument, 0 , 'h'} 
+	};	
+	
+	while ( (opt = getopt_long(argc, argv, "i:hv", long_options, &option_index)) != -1) {
+                if (opt == 'i') {
+			strcpy(ip , optarg);
+		}
+		if (opt == 'r'){
+			*vrid = atoi(optarg);
+		}
+		if (opt == 'p'){
+			*priority = atoi(optarg);
+		}
+		if (opt == 'c'){
+			*nb_p = atoi(optarg);
+		}
+		if (opt == 't'){
+			*time = atoi(optarg);
+		}
+		if (opt == 'v'){
+			*verbose = 1;
+		}
+		if (opt == 'h'){
+			help(argv[0]);
+			exit(0);
+		}		 		
+	}
+}
+	
 
 int main( int argc , char * argv[]) {
 
-	int errnum;
+	int errnum ;
+	int verbose = 0;
+	int time = 1; 
 	int vrrp_sd;
+	int i = 0;
+	int nb_p = 1;
+	int vrid_int ; 
+	char vrid = 100; 
+	char priority = 50;
 	struct sockaddr_in  * vrrpaddr ;
 	vrrphdr_t h_vrrp;
+	char ip[32] ;
 	
-	char * ip = argv[1];
-	printf("Ip = %s\n",ip);
+	bzero(ip,sizeof(ip));	
+	option( argc , argv , ip , &vrid , &priority , &nb_p , &time , & verbose);
+	if (ip[0] == '\0' ) {
+		help(argv[0]);
+		return 1;
+	}	
 	
 	vrrp_sd = socket(AF_INET , SOCK_RAW , IPPROTO_VRRP );
 	if (vrrp_sd < 0 ) {
@@ -61,27 +131,36 @@ int main( int argc , char * argv[]) {
 		return 1;
 	}
 	else {
-		printf("File descriptor %d\n" , vrrp_sd);
+		if (verbose == 1 ) { 
+			printf("File descriptor %d\n" , vrrp_sd);
+		}
 	}
 	
 	vrrpaddr = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in)); 
 	vrrpaddr->sin_family = AF_INET;
 	vrrpaddr->sin_port = 0; 
-	if (inet_pton(AF_INET, "192.168.1.1" , &(vrrpaddr->sin_addr)) == -1) {
+	if (inet_pton(AF_INET, ip , &(vrrpaddr->sin_addr)) == -1) {
 		printf("erreur inet_pton\n");
 	}
 
 	bzero(&h_vrrp ,sizeof(h_vrrp));
 	h_vrrp.vers_type = 33;
-	h_vrrp.vrid = 100;
-	h_vrrp.priority = 99;
-	h_vrrp.adver_int = 1;
-	h_vrrp.chksum = checksum(&h_vrrp, sizeof(h_vrrp));  	
-	if (sendto(vrrp_sd, &h_vrrp, sizeof(h_vrrp), 0, (struct sockaddr*)vrrpaddr,sizeof(*vrrpaddr))<= 0 ) {
-		printf("erreur\n");
-		errnum = errno;
-		fprintf(stderr, "Value of errno: %d\n", errno);
-		fprintf(stderr, "Error sendto: %s\n", strerror( errnum ));  
+	h_vrrp.vrid = vrid;
+	h_vrrp.priority = priority;
+	h_vrrp.adver_int = time;
+	h_vrrp.chksum = checksum(&h_vrrp, sizeof(h_vrrp));
+	while ( i < nb_p ){  	
+		if (sendto(vrrp_sd, &h_vrrp, sizeof(h_vrrp), 0, (struct sockaddr*)vrrpaddr,sizeof(*vrrpaddr))<= 0 ) {
+			printf("erreur\n");
+			errnum = errno;
+			fprintf(stderr, "Value of errno: %d\n", errno);
+			fprintf(stderr, "Error sendto: %s\n", strerror( errnum ));  
+		}
+		if (verbose == 1) {
+			printf("Paquet advert vrrp %d envoyé \n",i+1);
+		}
+		sleep(time);
+		i++;
 	}
 
 	close(vrrp_sd);	
